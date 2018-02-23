d606aeaae6d7e5c2b1db86a18876fefef6b15051
Sean Chalmers <sclhiannan@gmail.com>
2018-02-23 13:43:30 +1000

    Updated path API

--- src/Reflex/Dom/Widget/SVG/Types/Path.hs
+++ src/Reflex/Dom/Widget/SVG/Types/Path.hs
@@ -1,37 +1,81 @@
 {-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE NoMonomorphismRestriction  #-}
 {-# LANGUAGE OverloadedStrings          #-}
-module Reflex.Dom.Widget.SVG.Types.Path where
+{-# LANGUAGE TypeFamilies               #-}
+module Reflex.Dom.Widget.SVG.Types.Path
+  ( PathCommandType (..)
+  , PathCommandRelativity (..)
+  , SVG_Path (..)
+  , P (..)
+  , _SmthQuadBez
+  , _QuadBez
+  , _SmthCubicBez
+  , _CubicBez
+  , _MoveTo
+  , _LineTo
+  , _Horizontal
+  , _Vertical
+  , _ClosePath
+  , _PathComm
+  , _m
+  , _M
+  , _l
+  , _L
+  , _h
+  , _H
+  , _v
+  , _V
+  , _z
+  , _Z
+  , makePathProps
+  , pathCommandToText
+  ) where
 
-import           Control.Lens                         (Prism', prism, view,
-                                                       ( # ))
+import           Control.Lens                         (Prism', Rewrapped,
+                                                       Wrapped (..), iso, prism,
+                                                       to, ( # ), (^.), (^..),
+                                                       _1, _2, _Wrapped)
+
+import           Data.Map                             (Map)
 
 import           Data.List.NonEmpty                   (NonEmpty)
-import qualified Data.List.NonEmpty                   as NE
 
 import           Data.Semigroup                       (Semigroup (..), (<>))
 
 import           Data.Text                            (Text)
 import qualified Data.Text                            as Text
 
+import           Reflex.Dom                           ((=:))
+
 import           Reflex.Dom.Widget.SVG.Types.Internal (wrappedToText)
-import           Reflex.Dom.Widget.SVG.Types.Pos      (Pos, X, Y, _PosX, _PosY)
+import           Reflex.Dom.Widget.SVG.Types.Pos      (Pos, X, Y)
 
 -- | Do not consider any of this final or usable, it's a mess and needs work
 
+newtype P =
+  P (Pos X, Pos Y)
+
+instance P ~ t => Rewrapped P t
+
+instance Wrapped P where
+  type Unwrapped P = (Pos X, Pos Y)
+  _Wrapped' = iso (\(P x) -> x) P
+
 data PathCommandType
   -- Normal line movements
-  = MoveTo (Pos X) (Pos Y)
-  | LineTo (Pos X) (Pos Y)
+  = MoveTo P
+  | LineTo P
   | Horizontal (Pos X)
   | Vertical (Pos Y)
   -- Curves!
-  | SmthQuadBez (Pos X) (Pos Y)
-  | QuadBez (Pos X) (Pos Y) (Pos X) (Pos Y)
-  | SmthCubicBez (Pos X) (Pos Y) (Pos X) (Pos Y)
-  | CubicBez (Pos X) (Pos Y) (Pos X) (Pos Y) (Pos X) (Pos Y)
+  | SmthQuadBez P
+  | QuadBez P P
+  | SmthCubicBez P P
+  | CubicBez P P P
 
   | ClosePath
 
@@ -46,39 +90,45 @@ newtype SVG_Path =
   D (NonEmpty PathCommand)
   deriving Semigroup
 
-_SmthQuadBez :: Prism' PathCommandType (Pos X, Pos Y)
-_SmthQuadBez = prism (uncurry SmthQuadBez)
-  (\case SmthQuadBez x y -> Right (x,y)
-         x               -> Left x
+instance SVG_Path ~ t => Rewrapped SVG_Path t
+
+instance Wrapped SVG_Path where
+  type Unwrapped SVG_Path = (NonEmpty PathCommand)
+  _Wrapped' = iso (\(D x) -> x) D
+
+_SmthQuadBez :: Prism' PathCommandType P
+_SmthQuadBez = prism SmthQuadBez
+  (\case SmthQuadBez x1y1 -> Right x1y1
+         x                -> Left x
   )
 
-_QuadBez :: Prism' PathCommandType (Pos X, Pos Y, Pos X, Pos Y)
-_QuadBez = prism (\(x1,y1,x2,y2) -> QuadBez x1 y1 x2 y2)
-  (\case QuadBez x1 y1 x2 y2 -> Right (x1, y1, x2, y2)
-         x                   -> Left x
+_QuadBez :: Prism' PathCommandType (P, P)
+_QuadBez = prism (uncurry QuadBez)
+  (\case QuadBez x1y1 x2y2 -> Right (x1y1, x2y2)
+         x                 -> Left x
   )
 
-_SmthCubicBez :: Prism' PathCommandType (Pos X, Pos Y, Pos X, Pos Y)
-_SmthCubicBez = prism (\(x1,y1,x2,y2) -> SmthCubicBez x1 y1 x2 y2)
-  (\case SmthCubicBez x1 y1 x2 y2 -> Right (x1, y1, x2, y2)
+_SmthCubicBez :: Prism' PathCommandType (P, P)
+_SmthCubicBez = prism (uncurry SmthCubicBez)
+  (\case SmthCubicBez x1y1 x2y2 -> Right (x1y1, x2y2)
          x                        -> Left x
   )
 
-_CubicBez :: Prism' PathCommandType (Pos X, Pos Y, Pos X, Pos Y, Pos X, Pos Y)
-_CubicBez = prism (\(x1,y1,x2,y2,x3,y3) -> CubicBez x1 y1 x2 y2 x3 y3)
-  (\case CubicBez x1 y1 x2 y2 x3 y3 -> Right (x1, y1, x2, y2, x3, y3)
+_CubicBez :: Prism' PathCommandType (P, P, P)
+_CubicBez = prism (\(x1y1,x2y2,x3y3) -> CubicBez x1y1 x2y2 x3y3)
+  (\case CubicBez x1y1 x2y2 x3y3 -> Right (x1y1, x2y2, x3y3)
          x                          -> Left x
   )
 
-_MoveTo :: Prism' PathCommandType (Pos X, Pos Y)
-_MoveTo = prism (uncurry MoveTo)
-  (\case MoveTo x y -> Right (x, y)
-         x          -> Left x)
+_MoveTo :: Prism' PathCommandType P
+_MoveTo = prism MoveTo
+  (\case MoveTo xy -> Right xy
+         x         -> Left x)
 
-_LineTo :: Prism' PathCommandType (Pos X, Pos Y)
-_LineTo = prism (uncurry LineTo)
-  (\case LineTo x y -> Right (x, y)
-         x          -> Left x)
+_LineTo :: Prism' PathCommandType P
+_LineTo = prism LineTo
+  (\case LineTo xy -> Right xy
+         x         -> Left x)
 
 _Horizontal :: Prism' PathCommandType (Pos X)
 _Horizontal = prism Horizontal
@@ -102,42 +152,23 @@ notRelativePath, relativePath :: PathCommandType -> PathCommand
 relativePath pc = _PathComm # (pc, Relative)
 notRelativePath pc = _PathComm # (pc, NotRelative)
 
-_D :: Prism' SVG_Path (NonEmpty PathCommand)
-_D = prism D (\(D p) -> Right p)
+_m,_M,_l,_L :: Pos X -> Pos Y -> PathCommand
+_m x y = relativePath $ _MoveTo . _Wrapped # (x,y)
+_M x y = notRelativePath $ _MoveTo . _Wrapped # (x,y)
+_l x y = relativePath $ _LineTo . _Wrapped # (x,y)
+_L x y = notRelativePath $ _LineTo . _Wrapped # (x,y)
 
-_m :: Pos X -> Pos Y -> PathCommand
-_m x y = relativePath $ _MoveTo # (x,y)
-
-_M :: Pos X -> Pos Y -> PathCommand
-_M x y = notRelativePath $ _MoveTo # (x,y)
-
-_l :: Pos X -> Pos Y -> PathCommand
-_l x y = relativePath $ _LineTo # (x,y)
-
-_L :: Pos X -> Pos Y -> PathCommand
-_L x y = notRelativePath $ _LineTo # (x,y)
-
-_h :: Pos X -> PathCommand
+_h,_H :: Pos X -> PathCommand
 _h = relativePath . (_Horizontal #)
-
-_H :: Pos X -> PathCommand
 _H = notRelativePath . (_Horizontal #)
 
-_v :: Pos Y -> PathCommand
+_v,_V :: Pos Y -> PathCommand
 _v = relativePath . (_Vertical #)
-
-_V :: Pos Y -> PathCommand
 _V = notRelativePath . (_Vertical #)
 
-_z :: PathCommand
+_z,_Z :: PathCommand
 _z = relativePath (_ClosePath # ())
-
-_Z :: PathCommand
 _Z = notRelativePath (_ClosePath # ())
-
-rComm :: Text -> PathCommandRelativity -> Text
-rComm c' Relative = Text.toLower c'
-rComm c' _        = c'
 
 pathCommandToText
   :: PathCommandType
@@ -145,36 +176,35 @@ pathCommandToText
   -> Text
 pathCommandToText pc pcr =
   let
-    pt = view wrappedToText
-    pt2 x y = pt x <> "," <> pt y
+    rComm c' Relative = Text.toLower c'
+    rComm c' _        = c'
+
+    pt2 xy =
+      let
+        ptx = xy ^. _Wrapped . _1 . wrappedToText
+        pty = xy ^. _Wrapped . _2 . wrappedToText
+      in
+        ptx <> "," <> pty
   in
     case pc of
-      MoveTo x y ->
-        rComm "M" pcr        <> pt2 x y
-      LineTo x y ->
-        rComm "L" pcr <> " " <> pt2 x y
-      Horizontal x ->
-        rComm "H" pcr <> " " <> pt x
-      Vertical y ->
-        rComm "V" pcr <> " " <> pt y
+      MoveTo xy    -> rComm "M" pcr <> pt2 xy
+      LineTo xy    -> rComm "L" pcr <> " " <> pt2 xy
+      Horizontal x -> rComm "H" pcr <> " " <> x ^. wrappedToText
+      Vertical y   -> rComm "V" pcr <> " " <> y ^. wrappedToText
 
-      SmthQuadBez x y ->
-        rComm "T" pcr <> pt2 x y
-      QuadBez cpx1 cpy1 x y ->
-        rComm "Q" pcr <> pt2 cpx1 cpy1 <> pt2 x y
-      SmthCubicBez cpx2 cpy2 x y ->
-        rComm "S" pcr <> pt2 cpx2 cpy2 <> pt2 x y
-      CubicBez cpx1 cpy1 cpx2 cpy2 x y ->
-        rComm "C" pcr <> pt2 cpx1 cpy1 <> pt2 cpx2 cpy2 <> pt2 x y
+      SmthQuadBez xy              -> rComm "T" pcr <> pt2 xy
+      QuadBez cpxcpy1 xy          -> rComm "Q" pcr <> pt2 cpxcpy1 <> pt2 xy
+      SmthCubicBez cpxcpy2 xy     -> rComm "S" pcr <> pt2 cpxcpy2 <> pt2 xy
+      CubicBez cpxcpy1 cpxcpy2 xy -> rComm "C" pcr <> pt2 cpxcpy1 <> pt2 cpxcpy2 <> pt2 xy
 
-      ClosePath ->
-        rComm "Z" pcr
+      ClosePath -> rComm "Z" pcr
 
-s :: SVG_Path
-s = _D # NE.fromList
-  [ _M (_PosX # 3.0) (_PosY # 4.0)
-  , _L (_PosX # 10.0) (_PosY # 12.0)
-  , _L (_PosX # 15.0) (_PosY # 30.0)
-  , _z
-  ]
+makePathProps
+  :: SVG_Path
+  -> Map Text Text
+makePathProps p =
+  let
+    pcs = p ^.. _Wrapped . traverse . _PathComm . to (uncurry pathCommandToText)
+  in
+    "d" =: Text.unwords pcs
 
