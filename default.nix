6859ca9a638ba5b60fed92c7703898fd5212b6ce
Sean Chalmers <sclhiannan@gmail.com>
2018-03-28 13:39:54 +1000

    Fix default.nix to not blow up on Haddock phase for GHCJS build

--- default.nix
+++ default.nix
@@ -2,7 +2,16 @@
 , compiler ? "ghc"
 } :
 let
+  adjust-for-ghcjs = drv: {
+    doHaddock = false;
+  };
+
+  adjust = drv:
+    if compiler == "ghcjs"
+    then adjust-for-ghcjs drv
+    else drv;
+
   pkgs = reflex-platform.nixpkgs.pkgs;
-  drv = reflex-platform.${compiler}.callPackage ./reflex-dom-svg.nix {};
+  drv = pkgs.haskell.lib.overrideCabal (reflex-platform.${compiler}.callPackage ./reflex-dom-svg.nix {}) adjust;
 in
   drv
