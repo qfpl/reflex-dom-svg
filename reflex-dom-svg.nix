2fcc8d8a5d81f5b63baf180e22d7e18218e84871
Sean Chalmers <sclhiannan@gmail.com>
2018-05-28 17:26:40 +1000

    Updated nix expression

--- reflex-dom-svg.nix
+++ reflex-dom-svg.nix
@@ -1,12 +1,13 @@
-{ mkDerivation, base, containers, lens, reflex
-, reflex-dom-core, safe, stdenv, text
+{ mkDerivation, base, containers, lens, reflex, reflex-dom-core
+, safe, stdenv, text
 }:
 mkDerivation {
   pname = "reflex-dom-svg";
-  version = "0.1.2.0";
+  version = "0.3.0.0";
   src = ./.;
   libraryHaskellDepends = [
     base containers lens reflex reflex-dom-core safe text
   ];
+  description = "Reflex functions for SVG elements";
   license = stdenv.lib.licenses.bsd3;
 }
