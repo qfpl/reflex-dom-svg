01ca2be49e5fea63cc591490769860cdd71fdfaf
Sean Chalmers <sclhiannan@gmail.com>
2018-05-29 15:02:46 +1000

    Removed ghc-options & cpp-option
    Removed the `-dedupe` and `cpp-option` from the cabal file as it was
    affecting the building of documentation for end users.
    
    The `default.nix` is now simpler as there is no need for overrides.

--- default.nix
+++ default.nix
@@ -2,16 +2,6 @@
 , compiler ? "ghc"
 } :
 let
-  adjust-for-ghcjs = drv: {
-    doHaddock = false;
-  };
-
-  adjust = drv:
-    if compiler == "ghcjs"
-    then adjust-for-ghcjs drv
-    else drv;
-
-  pkgs = reflex-platform.nixpkgs.pkgs;
-  drv = pkgs.haskell.lib.overrideCabal (reflex-platform.${compiler}.callPackage ./reflex-dom-svg.nix {}) adjust;
+  drv = reflex-platform.${compiler}.callPackage ./reflex-dom-svg.nix {};
 in
   drv
